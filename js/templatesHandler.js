9672bd94da16de4a316ed732c405641c7db3d1a6
Inazio Claver <jclaver64@gmail.com>
2021-01-03 21:41:15 +0100

    Creacion de templates

--- js/templatesHandler.js
+++ js/templatesHandler.js
@@ -9,4 +9,38 @@
         };
     }); 
 
+    app.directive('central', function () {
+        return  {
+            restrict: 'E',
+            templateUrl: '/templates/central.html'
+        }
+    });
+
+    app.directive('presentation', function () {
+        return {
+            restrict: 'E',
+            templateUrl: '/templates/presentation.html'
+        };
+    });
+
+    app.directive('projects', function () {
+        return {
+            restrict: 'E',
+            templateUrl: '/templates/projects.html'
+        }
+    });
+
+    app.directive('me', function () {
+        return {
+            restrict: 'E',
+            templateUrl: '/templates/me.html'
+        }
+    });
+
+    app.directive('contact', function () {
+        return {
+            restrict: 'E',
+            templateUrl: '/templates/contact.html'
+        }
+    });
 })();
